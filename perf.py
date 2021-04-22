b466401d3c49c1a16b914f5853e6e18d62b71df3
Amos <apkendall10@gmail.com>
2021-04-22 16:57:21 -0500

    add performance script

--- perf.py
+++ perf.py
@@ -1,15 +1,33 @@
 import requests, time
+import pandas as pd
+from argparse import ArgumentParser
 
-url = f"http://localhost:8001/predict"
+parser = ArgumentParser()
+parser.add_argument('-n', dest='node', type=int,
+                    help='Process number to track output.', default=0)
 
-text = 'test'
-params = {'Input': text, 
+url = f"http://35.184.97.122/predict"
+
+def make_request(text):
+    params = {'Input': text, 
           'Category': 'NLP',
           'Task': 'fakenews'}
+    requests.post(url, json=params)
 
-for epoch in range(10):
-    tic = time.time()
-    for _ in range(100):
-        requests.post(url, json=params)
-    elapsed = round(time.time()-tic,3)
-    print(f'Epoch {epoch+1}: {elapsed}, {100/elapsed}')
+def main(node):
+    latencies = []
+    df = pd.read_csv('all.csv')
+    start = time.time()
+    for i in range(len(df)):
+        row = df.iloc[i]
+        tic = time.time()
+        make_request(row.text)
+        latencies.append(round(time.time() - tic,4))
+    total = round(time.time() - start,4)
+    pd.Series(latencies).to_csv(f'{node}_latenices.csv')
+    pd.Series([total]).to_csv(f'{node}_total.csv')
+
+if __name__ == '__main__':
+    
+    args = parser.parse_args()
+    main(args.node)
